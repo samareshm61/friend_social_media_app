1a9dfadba0fc8aa2dfd85558bfa385f646554650
samareshm61 <100060477+samareshm61@users.noreply.github.com>
2023-09-26 09:05:03 +0530

    mode view controller set up also manual authentication setup

---
+++ controllers/users_controller.js
@@ -0,0 +1,46 @@
+const User = require("../models/user");
+module.exports.profile = function (req, res) {
+  return res.render("user_profile", {
+    title: "User Profile",
+  });
+};
+
+// render the sign up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Codeial | Sign Up",
+  });
+};
+
+// render the sign in page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Codeial | Sign In",
+  });
+};
+
+// get the sign up data
+module.exports.create = async function (req, res) {
+  try {
+    if (req.body.password !== req.body.confirm_password) {
+      return res.redirect("back");
+    }
+
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (!existingUser) {
+      const user = await User.create(req.body);
+      return res.redirect("/users/sign-in");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (err) {
+    console.error("Error in signing up:", err);
+    return res.redirect("back");
+  }
+};
+
+// sign in and create a session for the user
+module.exports.createSession = function (req, res) {
+  // TODO later
+};
